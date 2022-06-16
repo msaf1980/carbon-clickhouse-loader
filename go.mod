6f9e7fa2e20bed62cf84f4805f6e493c8941b1dd
Safronov Michail <msaf1980@gmail.com>
2022-06-16 17:35:40 +0500

    clickhouse-go tagged driver

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/msaf1980/carbon-clickhouse-loader
 go 1.16
 
 require (
+	github.com/ClickHouse/clickhouse-go/v2 v2.0.15
 	github.com/mailru/go-clickhouse/v2 v2.0.0
 	github.com/maruel/natural v1.1.0
 	github.com/msaf1980/go-stringutils v0.0.15
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.2
 	github.com/tevino/abool v1.2.0
 	github.com/tevino/abool/v2 v2.1.0
 )
---
+++ pkg/driver/std/driver.go
@@ -0,0 +1,117 @@
+package mailru
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/msaf1980/carbon-clickhouse-loader/pkg/driver"
+	"github.com/msaf1980/carbon-clickhouse-loader/pkg/tags"
+	"github.com/tevino/abool"
+)
+
+type TaggedDriver struct {
+	address string
+	table   string
+
+	flushSize uint // metrics max size in bytes
+
+	size    uint                 // size (for flush detect)
+	metrics []driver.MetricIndex // metrics buffer
+
+	isRunning *abool.AtomicBool
+}
+
+func NewTaggedDriver(address, table string, flushSize uint) *TaggedDriver {
+	if len(address) == 0 {
+		address = "127.0.0.1:9000"
+	}
+	return &TaggedDriver{
+		address:   address,
+		table:     table,
+		flushSize: flushSize,
+		metrics: make(
+			[]driver.MetricIndex,
+			0, flushSize/100, // some evristic: size / avg metric length
+		),
+	}
+}
+
+func (d *TaggedDriver) Queued() uint {
+	return d.size
+}
+
+func (d *TaggedDriver) Write(m driver.MetricIndex) (time.Duration, uint, error) {
+	var duration time.Duration
+	var n uint
+	var err error
+	// fmt.Printf("%s %v\n", m.Metric, m.Date)
+	if d.size >= d.flushSize {
+		if duration, n, err = d.Flush(); err != nil {
+			return duration, n, err
+		}
+	}
+
+	if len(m.Metric) > 0 {
+		d.metrics = append(d.metrics, m)
+		d.size += uint(len(m.Metric))
+	} else {
+		return d.Flush()
+	}
+
+	return duration, n, nil
+}
+
+func (d *TaggedDriver) Flush() (time.Duration, uint, error) {
+	var n uint
+	start := time.Now()
+	if d.size > 0 {
+		conn, err := sql.Open("clickhouse", "clickhouse://"+d.address)
+		if err != nil {
+			return 0, 0, err
+		}
+		tx, err := conn.Begin()
+		if err != nil {
+			return 0, 0, err
+		}
+		batch, err := tx.Prepare("INSERT INTO " + d.table + " (Date, Tag1, Path, Tags, Version)")
+		if err != nil {
+			return 0, 0, err
+		}
+
+		// fmt.Println("FLUSH")
+		for _, m := range d.metrics {
+			if path, tags, err := tags.TagsParse(m.Metric); err != nil {
+				fmt.Fprintf(os.Stderr, "invalid metric '%s': %v", m.Metric, err)
+			} else {
+				// fmt.Printf("%s %+v %v\n", name, tags, m.Date)
+				for _, tag1 := range tags {
+					if _, err := batch.Exec(
+						m.Date,
+						tag1,
+						path,
+						tags,
+						uint32(0),
+					); err != nil {
+						return time.Since(start), 0, err
+					}
+				}
+				n++
+			}
+		}
+
+		if err := tx.Commit(); err != nil {
+			return time.Since(start), 0, err
+		}
+
+		d.metrics = d.metrics[:0]
+		d.size = 0
+	}
+	return time.Since(start), n, nil
+}
+
+func (d *TaggedDriver) Close() error {
+	return nil
+}
